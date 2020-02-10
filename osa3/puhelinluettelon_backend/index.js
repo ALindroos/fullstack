53d8e17ad54e3928100d1013bb1d52abe7042026
ALindroos <adiel.lindroos@helsinki.fi>
2020-02-10 19:32:11 +0200

    linting fixed

--- osa3/puhelinluettelon_backend/index.js
+++ osa3/puhelinluettelon_backend/index.js
@@ -30,10 +30,10 @@ app.get('/api/persons/:id', (request, response, next) => {
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
-  .then(result => {
-    response.status(204).end()
-  })
-  .catch(error => next(error))
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
@@ -47,19 +47,19 @@ app.post('/api/persons', (request, response, next) => {
   person.save()
     .then(savedPerson => {
       response.json(savedPerson.toJSON())
-  })
-  .catch(error => next(error))
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
   const body = request.body
-  
+
   const person = {
     name: body.name,
     number: body.number,
   }
 
-  Person.findByIdAndUpdate(request.params.id, person, {new: true })
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
     .then(updatedPerson => {
       response.json(updatedPerson.toJSON())
     })
@@ -67,7 +67,7 @@ app.put('/api/persons/:id', (request, response, next) => {
 })
 
 
-app.get('/info', (request, response, next) => {
+app.get('/info', (request, response) => {
   const date = new Date()
   Person.find({}).then(persons => {
     response.send(
@@ -75,13 +75,12 @@ app.get('/info', (request, response, next) => {
       `<p>${date}</p>`
     )
   })
-  .catch(error => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
   console.log(error.message)
 
-  if (error.name === 'CastError' && error.kind == 'ObjectId') {
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
