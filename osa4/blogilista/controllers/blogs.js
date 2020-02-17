bfd2ccef053c3288652b51af17835824fbea153f
ALindroos <adiel.lindroos@helsinki.fi>
2020-02-17 09:24:10 +0200

    ex 4.14

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -23,4 +23,31 @@ blogsRouter.post('/', async (request, response, next) => {
   }
 })
 
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } catch (exception) {
+    next(exception)
+  }
+})
+
+blogsRouter.put('/:id', async (request, response, next) => {
+  const body = request.body
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+
+  try {
+    const updBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    response.json(updBlog)
+  } catch (exception) {
+    next(exception)
+  }
+})
+
 module.exports = blogsRouter
