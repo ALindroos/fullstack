b46603642c0e0dd9498ce0903d3c3bf3e499a188
ALindroos <adiel.lindroos@helsinki.fi>
2020-02-17 11:16:28 +0200

    users functionality

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
+const usersRouter = require('./controllers/users')
 
 logger.info('connecting to MongoDB')
 
@@ -22,6 +23,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.errorHandler)
 
---
+++ osa4/blogilista/controllers/users.js
@@ -0,0 +1,29 @@
+const bcryptjs = require('bcryptjs')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.post('/', async (request, response, next) => {
+  const body = request.body
+
+  const salt = bcryptjs.genSaltSync(10)
+  const passwordHash = await bcryptjs.hash(body.password, salt)
+
+  const user = new User({
+    username: body.username,
+    name: body.name,
+    passwordHash: passwordHash,
+  })
+  try {
+    const savedUser = await user.save()
+    response.json(savedUser)
+  } catch (execption) {
+    next(execption)
+  }
+})
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users.map(u => u.toJSON()))
+})
+
+module.exports = usersRouter
