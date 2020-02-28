46119280226db63874cdeeac71b7c9b602ed4dbb
ALindroos <adiel.lindroos@helsinki.fi>
2020-02-28 10:31:24 +0200

    ex 6.2

--- osa6/unicafe-redux/src/index.js
+++ osa6/unicafe-redux/src/index.js
@@ -5,22 +5,59 @@ import reducer from './reducer'
 
 const store = createStore(reducer)
 
+const Statistics = () => {
+  const state = store.getState()
+  const vals = Object.values(state)
+  const total = vals.reduce((a,b) => a + b)
+  const avg = (state.good - state.bad) / (total)
+  const pos = ((state.good / total) * 100 ) + '%'
+  
+  return(
+    <div>
+      <p>average: {avg}</p>
+      <p>positive: {pos}</p>
+      <p>total: {total}</p>
+    </div>
+  )
+  
+}
+
 const App = () => {
+
   const good = () => {
     store.dispatch({
       type: 'GOOD'
     })
   }
+  const ok = () => {
+    store.dispatch({
+      type: 'OK'
+    })
+  }
+  const bad = () => {
+    store.dispatch({
+      type: 'BAD'
+    })
+  }
+  const zero = () => {
+    store.dispatch({
+      type: 'ZERO'
+    })
+  }
+
 
   return (
     <div>
       <button onClick={good}>hyvä</button> 
-      <button>neutraali</button> 
-      <button>huono</button>
-      <button>nollaa tilastot</button>
+      <button onClick={ok}>neutraali</button> 
+      <button onClick={bad}>huono</button>
+      <button onClick={zero}>nollaa tilastot</button>
       <div>hyvä {store.getState().good}</div>
-      <div>neutraali</div>
-      <div>huono</div>
+      <div>neutraali {store.getState().ok}</div>
+      <div>huono {store.getState().bad}</div>
+
+      <h2>Statistics:</h2>
+      <Statistics />
     </div>
   )
 }
